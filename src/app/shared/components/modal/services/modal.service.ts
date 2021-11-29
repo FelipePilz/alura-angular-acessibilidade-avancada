8f295c0f868b463732de7e5c05c524392a07a0a5
FelipePilz <felipevilvertpilz@gmail.com>
2021-11-29 19:57:05 -0300

    Definindo interface para a API Modal

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, TemplateRef } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ModalService {
   constructor() {}
+
+  public open(config: ModalConfig) {
+    console.log('open called');
+    return new ModalRef();
+  }
+}
+
+export interface ModalConfig {
+  templateRef: TemplateRef<any>;
+  title: string;
+}
+
+export class ModalRef {
+  public close(): void {
+    console.log('close called');
+  }
 }
