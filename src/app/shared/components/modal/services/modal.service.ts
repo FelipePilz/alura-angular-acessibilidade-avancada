d810ff4da5bb20b2cfc035fc30bd3192d5d07033
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-04 20:57:50 -0300

    Fechando o modal ao clicar no overlay

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -8,6 +8,7 @@ import {
   Injector,
 } from '@angular/core';
 import { ModalConfig } from '../interfaces/modal-config';
+import { ModalRef } from '../models/modal-ref';
 
 @Injectable({
   providedIn: 'root',
@@ -29,19 +30,12 @@ export class ModalService {
     componentRef.instance.config = config;
     console.log('open called');
     this.bodyInjector.stackBeforeAppRoot(componentRef);
-    return new ModalRef(componentRef);
+    const modalRef = new ModalRef(componentRef);
+    componentRef.instance.modalRef = modalRef;
+    return modalRef;
   }
 
   private createComponentRef(): ComponentRef<ModalComponent> {
     return this.componentFactory.create(this.injector);
-  }
-}
-
-export class ModalRef {
-  constructor(private componentRef: ComponentRef<ModalComponent>) {}
-
-  public close(): void {
-    console.log('close called');
-    this.componentRef.destroy();
   }
 }
