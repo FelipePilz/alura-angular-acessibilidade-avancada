ac48d0399b72042ece7f3ca2107384d85b97b7aa
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-05 18:24:28 -0300

    Inserindo a diretiva FocusTrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import {
-  ModalService,
-  ModalRef,
-} from './shared/components/modal/services/modal.service';
+import { ModalService } from './shared/components/modal/services/modal.service';
 import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { ModalRef } from './shared/components/modal/models/modal-ref';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/shared/directives/focus-trap/focus-trap.directive.ts
@@ -0,0 +1,24 @@
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appFocusTrap]',
+})
+export class FocusTrapDirective implements AfterViewInit {
+  private firstFocusableElement: HTMLElement = null;
+  private lastFocusableElement: HTMLElement = null;
+
+  constructor(private elementRef: ElementRef<any>) {}
+
+  ngAfterViewInit(): void {
+    const focusableElements = this.elementRef.nativeElement.querySelectorAll(`
+        [tabindex]:not([tabindex="-1"]),
+        a[href]:not([disabled]),
+        button:not([disabled]),
+        textarea:not([disabled]),
+        input:not([disabled]),
+        select:not([disabled])`);
+    this.firstFocusableElement = focusableElements[0];
+    this.lastFocusableElement = focusableElements[focusableElements.length];
+    this.firstFocusableElement.focus();
+  }
+}
